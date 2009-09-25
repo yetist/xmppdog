3767194a7e7c232102cef94688efa24dc8bbd656
yetist <yetist@136f7b1e-3484-11de-a9fe-ad1e22fc3a8a>
2009-09-25 16:33:32 +0000

    可配制插件是否启用，尝试解决过夜死

--- trunk/xmppdog/main.py
+++ trunk/xmppdog/main.py
@@ -25,6 +25,7 @@ import ConfigParser
 import sys, os
 import string
 import time
+import datetime
 import traceback
 
 import pyxmpp
@@ -41,8 +42,13 @@ class Application(JabberClient):
         self.show_info = None
         self.admin = None
         self.jid = None
+        self.exiting=0
+        self.disconnecting=0
         self.password = None
         self.cfg = ConfigParser.ConfigParser()
+        self.allow_plugins = []
+        self.all_plugins = []
+        self.disconnect_timeout = 2
         self.read_cfg()
         self.base_dir = base_dir
         home = os.environ.get("HOME", "")
@@ -52,21 +58,6 @@ class Application(JabberClient):
         self.plugins = {}
         self.plugin_modules = {}
         self.room_manager=None
-
-#        # if bare JID is provided add a resource -- it is required
-#        if not jid.resource:
-#            jid=JID(jid.node, jid.domain, "xmppdog")
-#
-#        # setup client with provided connection information
-#        # and identity data
-#        JabberClient.__init__(self, jid, password,
-#                disco_name="XMPP Bot: xmppdog", disco_type="bot")
-#
-#        # add the separate components
-#        self.interface_providers = [
-#            VersionHandler(self),
-#            EchoHandler(self),
-#            ]
 
     def read_cfg(self):
         """ Read application config file. """
@@ -80,6 +71,10 @@ class Application(JabberClient):
                       )
         self.password = unicode(self.cfg.get('login', 'pass'), 'iso-8859-2')
         self.auth_methods = string.split(self.cfg.get('login', 'auth_methods'))
+        self.disconnect_timeout = self.cfg.getfloat('login','disconnect_timeout')
+        for (n, v) in self.cfg.items("plugins"):
+            if int(v):
+                self.allow_plugins.append(n)
         
     def run(self):
         """ 
@@ -112,6 +107,10 @@ class Application(JabberClient):
         """ 
         Load the plugin. 
         """
+        if name not in self.allow_plugins:
+            self.info("Skipping blocked plugin %s" % (name,))
+            return
+
         try:
             mod = self.plugin_modules.get(name)
             if mod:
@@ -170,6 +169,7 @@ class Application(JabberClient):
                         continue
                     name = os.path.join(plugin_file[:-3])
                     if not self.plugins.has_key(name):
+                        self.all_plugins.append(name)
                         self.info("  %s" % (name,))
                         self._load_plugin(name)
         except StandardError:
@@ -219,12 +219,6 @@ class Application(JabberClient):
         """
         return self.plugins[name]
 
-    def stream_state_changed(self,state,arg):
-        """This one is called when the state of stream connecting the component
-        to a server changes. This will usually be used to let the user
-        know what is going on."""
-        print "*** State changed: %s %r ***" % (state,arg)
-
     def print_roster_item(self,item):
         print "call print_roster_item"
         if item.name:
@@ -245,31 +239,76 @@ class Application(JabberClient):
         print u"Roster item updated:"
         self.print_roster_item(item)
 
+    def exit_time(self):
+        if not self.exiting:
+            return 0
+        if not self.get_stream():
+            return 1
+        if self.exiting>time.time() + self.disconnect_timeout:
+            return 1
+        return 0
+
     def idle(self):
         stream=self.get_stream()
-        if stream:
-            stream.idle()
+        while not self.exit_time():
+            self.state_changed.acquire()
+            stream=self.get_stream()
+            if not stream:
+                self.state_changed.wait(1)
+                stream=self.get_stream()
+            self.state_changed.release()
+            if not stream:
+                continue
+            try:
+                act = self.stream.loop_iter(1)
+                if not act:
+                    self.stream.idle()
+            except (pyxmpp.FatalStreamError,pyxmpp.StreamEncryptionRequired),e:
+                self.state_changed.acquire()
+                try:
+                    if isinstance(e, pyxmpp.exceptions.TLSError):
+                        self.error(u"You may try disabling encryption: /set tls_enable false")
+                    try:
+                        self.get_stream.close()
+                    except:
+                        pass
+                    stream=None
+                    self.state_changed.notify()
+                finally:
+                    self.state_changed.release()
+            except pyxmpp.StreamError,e:
+                self.disconnecting = 1
+                self.disconnect()
+            except (KeyboardInterrupt,SystemExit),e:
+                self.exit_request(unicode(str(e)))
+            except:
+                raise
 
-#    def session_started(self):
-#        self.request_roster()
-#        p=Presence()
-#        self.stream.send(p)
-#        self.room_manager=muc.MucRoomManager(self.stream)
-#        self.room_manager.set_handlers()
-#        for room_id in conf.room.keys():
-#            room_jid=pyxmpp.JID(unicode(room_id))
-#            if room_jid.resource or not room_jid.node:
-#                self.error("Bad room JID")
-#                return
-#            rs=self.room_manager.get_room_state(room_jid)
-#            if rs and rs.joined:
-#                room_handler=rs.handler
-#            else:
-#                room_handler=Room(conf, room_jid, self.stream.me)
-#                self.room_manager.join(room_jid, conf.room[room_id], room_handler)
+    def force_disconnect(self):
+        self.lock.acquire()
+        self.stream.close()
+        self.stream=None
+        self.lock.release()
+        self.disconnected()
+
+    def exit_request(self,reason):
+        if self.stream:
+            if self.disconnecting:
+                self.force_disconnect()
+            else:
+                self.disconnecting=1
+                if reason:
+                    self.info(u"Disconnecting (%s)..." % (reason,))
+                else:
+                    self.info(u"Disconnecting...")
+                time.sleep(5)
+                self.disconnect()
+        self.state_changed.acquire()
+        self.exiting=time.time()
+        self.state_changed.notify()
+        self.state_changed.release()
 
     def cmd_sudo(self, stanza, command):
-        print "command is :", command
         target = JID(stanza.get_from())
         if   len(command) == 1:
             msg = self.cmd_help()
@@ -294,8 +333,15 @@ class Application(JabberClient):
             elif (command[1] == "list" and command[2] == "plugins"):
                 msg = 'current plugins are:\n' + "\n".join(self.plugins.keys())
                 self.stream.send(Message(to_jid=target, body=msg))
+            elif (command[1] == "list" and command[2] == "allplugins"):
+                msg = 'all plugins are:\n' + "\n".join(self.all_plugins)
+                self.stream.send(Message(to_jid=target, body=msg))
         elif len(command) == 4:
             plugin = command[3]
+            if plugin not in self.allow_plugins:
+                msg = 'plugin ' + plugin + ' is blocked'
+                self.stream.send(Message(to_jid=target, body=msg))
+                return
             if (command[1] == "load" and command[2] == "plugin"):
                 self.load_plugin(plugin)
                 self.get_plugin(plugin).session_started(self.stream)
@@ -330,9 +376,6 @@ class Application(JabberClient):
                 command = body.split()
                 if (command[0] == ">sudo"):
                     self.cmd_sudo(stanza, command)
-               # elif (command[0] == "/help"):
-               #     msg = self.cmd_help()
-               #     self.stream.send(Message(to_jid=target, body=msg))
                 else:
                     self.plugins_message_chat(stanza)
         return 1
@@ -402,7 +445,7 @@ class Application(JabberClient):
         elif state == "binding":
             self.info("Binding to resource %s..." % (arg,))
         elif state == "authorized":
-            self.info("Authorized as %s." % (arg,))
+            self.info("Authorized as %s." % (arg.as_utf8(),))
         elif state == "tls connecting":
             self.info("Doing TLS handhake with %s." % (arg,))
     
@@ -421,14 +464,6 @@ class Application(JabberClient):
         for i in sub_cmd:
             lst.append(" ".join([cmd, i]))
         return unicode("\n".join(lst), 'iso-8859-2')
-#        for plugin in self.plugins.values():
-#            try:
-#                lst.append(plugin.cmd_help())
-#            except StandardError:
-#                self.print_exception()
-#                self.info("Plugin call failed")
-#        msg = unicode("\n".join(lst))
-#        self.stream.send(Message(to_jid=target, body=msg))
     
     def error(self, error_text):
         """
@@ -468,17 +503,8 @@ def main(base_dir):
     """
     Run the application.
     """ 
-    #libxml2.debugMemory(1)
-    
     app = Application(base_dir, 'xmppdog.cfg')
     
     app.run()
-    
-    #libxml2.cleanupParser()
-    #if libxml2.debugMemory(1) == 0:
-    #    print "OK"
-    #else:
-    #    print "Memory leak %d bytes" % (libxml2.debugMemory(1))
-    #    libxml2.dumpMemory()
 
 # vi: sts=4 et sw=4
