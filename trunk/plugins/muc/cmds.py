4f9d35937ff9df43263fa1218eb50b27a9d7d717
yetist <yetist@136f7b1e-3484-11de-a9fe-ad1e22fc3a8a>
2011-05-18 10:41:47 +0000

    fixed fetch function

--- trunk/plugins/muc/cmds.py
+++ trunk/plugins/muc/cmds.py
@@ -103,24 +103,26 @@ def cmd_usage(myself, params):
 def cmd_fetch(myself, params):
     "订阅，执行此命令后，聊天室的消息将会转发给自己一份"
 
+    myjid = str(params['jid'])
     nick = params["nick"]
-    if nick not in myself.fetchlist:
-        myself.fetchlist.append(nick)
+    if myjid not in myself.fetchlist:
+        myself.fetchlist.append(myjid)
 
         num = len(myself.fetchlist)
-        msg = u"/me \"%s\"订阅了本聊天室的消息，目前共有 %d 个订阅者，他们是%s。" % (nick, num, " ".join(myself.fetchlist))
+        msg = u"/me \"%s\"订阅了本聊天室的消息，目前共有 %d 个订阅者，他们是\n%s。" % (nick, num, "\n".join(myself.fetchlist))
 
         myself.send2room(msg)
-        myself.send2one(params['nick'], "已经订阅成功")
+        myself.send2one(nick, "已经订阅成功")
     else:
-        myself.send2one(params['nick'], "不能重复订阅聊天室消息")
+        myself.send2one(nick, "不能重复订阅聊天室消息")
 
 def cmd_unfetch(myself, params):
     "取消订阅，执行此命令后，聊天室的消息将不再转发给自己"
 
+    myjid = str(params['jid'])
     nick = params["nick"]
-    if nick in myself.fetchlist:
-        myself.fetchlist.remove(nick)
+    if myjid in myself.fetchlist:
+        myself.fetchlist.remove(myjid)
         num = len(myself.fetchlist)
         if num == 0:
             msg = u"/me \"%s\"取消了订阅本聊天室的消息，目前没有订阅者。" % nick
@@ -128,9 +130,9 @@ def cmd_unfetch(myself, params):
             msg = u"/me \"%s\"取消了订阅本聊天室的消息，目前共有 %d 个订阅者，他们是%s。" % (nick, num, " ".join(myself.fetchlist))
 
         myself.send2room(msg)
-        myself.send2one(params['nick'], "成功取消订阅")
+        myself.send2one(nick, "成功取消订阅")
     else:
-        myself.send2one(params['nick'], "你没有订阅过聊天室消息")
+        myself.send2one(nick, "你没有订阅过聊天室消息")
 
 def cmd_unblockme(myself, params):
     "恢复抓取自己发送的链接标题"
